6b17627ca01dc4c60d22a4b78da1152b0b123b72
MvLeronn <marcos23_correia@hotmail.com>
2025-04-07 20:00:15 -0300

    feat: adicionar validação ao formulário de IMC

--- app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
@@ -1,11 +1,38 @@
 package co.mvleronn.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if(!validate()){
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
+
+    private fun validate(): Boolean{
+        // nao pode inserir valores nulos/vazio e nao pode 0
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && editWeight.text.toString() != "0"
+                && editHeight.text.toString() != "0")
+    }
+
 }
