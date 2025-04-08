a054d49627d72784289e091a918b08eb8378b400
MvLeronn <marcos23_correia@hotmail.com>
2025-04-08 17:18:24 -0300

    feat: adicionar função para mostrar em forma de texto seu IMC

--- app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 
 class ImcActivity : AppCompatActivity() {
@@ -24,8 +25,37 @@ class ImcActivity : AppCompatActivity() {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            val weight = editWeight.text.toString().toInt()
+            val height = editHeight.text.toString().toInt()
+
+            val result = calculateImc(weight, height)
+
+            val imcResponseId = imcResponse(result)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_LONG).show()
         }
     }
+
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        // peso / (altura * altura)
+        // altura/100 para transformar em m
+        return weight / ((height/100.0) * (height/100.0))
+    }
+
 
     private fun validate(): Boolean{
         // nao pode inserir valores nulos/vazio e nao pode 0
