32715098f8b607604ab5009124dcf9fa34e4fb19
MvLeronn <marcos23_correia@hotmail.com>
2025-04-14 17:11:48 -0300

    feat: esconder teclado ao perder o foco do input usando InputMethodManager

--- app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/mvleronn/fitnesstracker/ImcActivity.kt
@@ -1,7 +1,10 @@
 package co.mvleronn.fitnesstracker
 
+import android.content.Context
 import android.content.DialogInterface
+import android.inputmethodservice.InputMethodService
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -45,6 +48,9 @@ class ImcActivity : AppCompatActivity() {
                 }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
