63be0749827f704b1962b8f63df6be6fe1efad9f
MvLeronn <marcos23_correia@hotmail.com>
2025-04-14 18:55:48 -0300

    feat: implementar RecyclerView com adapter na MainActivity

--- app/src/main/java/co/mvleronn/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/mvleronn/fitnesstracker/MainActivity.kt
@@ -1,23 +1,50 @@
 package co.mvleronn.fitnesstracker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnImc: LinearLayout
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
+        val adapter = MainAdapter()
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
 
-        btnImc.setOnClickListener {
-            val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
+        // classe para administrar a recyclerview e suas celulas (os sues layouts de itens)
+        // Adapter ->
+    }
+
+    private inner class MainAdapter: RecyclerView.Adapter<MainViewHolder>() {
+        // Qual é o layout XML da celula especifica(item)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
         }
+
+        // método disparado toda vez que houver uma rolagem na tela e for
+        // necessário trocar o conteudo da celula
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+
+        }
+        // quantas celulas essa listagem terá
+        override fun getItemCount(): Int {
+            return 15
+        }
+
+    }
+
+    // é a classe da célula em si
+    private class MainViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
     }
 }
